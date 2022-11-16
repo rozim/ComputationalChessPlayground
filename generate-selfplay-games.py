bf0d1b03511379b691b25bd505900fc8855b66d1
Dave Spencer <david.spencerian@gmail.com>
2022-11-15 22:18:03 -0800

    seems decent now

--- generate-selfplay-games.py
+++ generate-selfplay-games.py
@@ -10,7 +10,6 @@
 
 import datetime
 import sys, os
-import random
 import time
 from random import random, choice
 
@@ -25,7 +24,7 @@ from absl import logging
 
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('max_game_ply', 100, '')
+flags.DEFINE_integer('max_ply', 100, '')
 flags.DEFINE_integer('num_games', 1, 'Number of games to generate')
 flags.DEFINE_integer('hash', 16, '')
 flags.DEFINE_integer('threads', 1, '')
@@ -33,6 +32,11 @@ flags.DEFINE_string('fen', 'rnbqkb1r/pppppppp/8/6B1/3Pn2P/8/PPP1PPP1/RN1QKBNR b 
 
 flags.DEFINE_float('time', 30.0, 'Time in seconds for entire game')
 flags.DEFINE_float('inc', 1.0, 'Increment in seconds')
+
+flags.DEFINE_integer('multipv', 1, '')
+flags.DEFINE_integer('threshold', 0, 'score delta')
+flags.DEFINE_float('pct', 0.25, 'How often to choose a non-optimal move with the threshold')
+
 STOCKFISH = './stockfish'
 PCT_RANDOM = 0.25
 
@@ -42,19 +46,32 @@ def play_game(engine, starting_fen):
   remaining_time = [FLAGS.time, FLAGS.time]
   while board.outcome() is None:
     ply += 1
-    if FLAGS.max_game_ply and ply >= FLAGS.max_game_ply:
+    if FLAGS.max_ply and ply >= FLAGS.max_ply:
       break
 
     t1 = time.time()
-    res = engine.analyse(board, chess.engine.Limit(white_clock=remaining_time[WHITE],
+    multi = engine.analyse(board, chess.engine.Limit(white_clock=remaining_time[WHITE],
                                                    black_clock=remaining_time[BLACK],
                                                    white_inc=FLAGS.inc,
-                                                   black_inc=FLAGS.inc))
+                                                   black_inc=FLAGS.inc),
+                         multipv=FLAGS.multipv)
+    scores = [m['score'].white().score(mate_score=10000) for m in multi]
+
+    alt = []
+    for i in range(1, len(scores)):
+      if abs(scores[i] - scores[0]) < FLAGS.threshold:
+        alt.append(multi[i]['pv'][0])
+
+    if not multi[0]['score'].white().is_mate() and len(alt) > 0 and random() < FLAGS.pct:
+      move = choice(alt)
+    else:
+      move = multi[0]['pv'][0]
     dt = time.time() - t1
-    move = res['pv'][0]
+
     remaining_time[board.turn] -= dt
     remaining_time[board.turn] += FLAGS.inc
-    print(f'MOVE: {move}, {remaining_time[WHITE]:.1f} {remaining_time[BLACK]:.1f} {dt:.1f} {board.fen()}')
+    # print(f'MOVE: {move}, {remaining_time[WHITE]:.1f} {remaining_time[BLACK]:.1f} {dt:.1f} {scores} {alt} {board.fen()}')
+
     board.push(move)
   return board
 
@@ -75,6 +92,7 @@ def generate_game(board, elapsed, starting_fen, xround):
   game.headers['X-Duration'] = f'{elapsed:.1f}s'
   game.headers['X-Time-Sec'] = str(FLAGS.time)
   game.headers['X-Inc-Sec'] = str(FLAGS.inc)
+  game.headers['X-Threshold'] = str(FLAGS.threshold)
 
   node = game
   for move in board.move_stack:
@@ -94,7 +112,8 @@ def main(argv):
     final_board = play_game(engine, FLAGS.fen)
     dt = time.time() - t1
     game = generate_game(final_board, dt, FLAGS.fen, n + 1)
-    print(str(game))
+    print(f'Game {n} {dt:.1f}s ply={len(final_board.move_stack)} {final_board.outcome()}')
+    # print(str(game))
 
     f_pgn.write(str(game) + '\n\n')
     f_pgn.flush()
